84a1b797c846fb27e2fb7c34f2e4c259554b7ebb
Gabe Alena <slabsk@mail.ru>
2022-07-06 16:52:50 +0500

    changed cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors, celebrate, Joi } = require('celebrate');
+const cors = require('cors');
 
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
@@ -10,7 +11,6 @@ const auth = require('./middlewares/auth');
 const NotFound = require('./errors/NotFound');
 const { serverError } = require('./errors/serverError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./cors/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -18,11 +18,10 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
 app.use(express.json());
+app.use(cors({ credentials: true, origin: ['https://localhost:3000', 'http://localhost3000', 'https://gabealena.students.nomoredomains.xyz', 'http://gabealena.students.nomoredomains.xyz'] }));
 app.use(cookieParser());
 
 app.use(requestLogger);
-
-app.use(cors());
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
