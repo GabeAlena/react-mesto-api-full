9e4833c61039d7dbdba8ceff7095d7abce737190
Gabe Alena <slabsk@mail.ru>
2022-07-11 12:14:50 +0500

    try to set jwt 19

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -19,7 +19,7 @@ class Api {
         return fetch(`${this._baseUrl}/users/me`, {
             method: 'GET',
             headers: this._headers,
-            credentials: 'include'
+            /* credentials: 'include' */
         })
         .then(this._checkResponse)
     }
@@ -29,7 +29,7 @@ class Api {
         return fetch(`${this._baseUrl}/cards`, {
             method: 'GET',
             headers: this._headers,
-            credentials: 'include'
+            /* credentials: 'include' */
         })
         .then(this._checkResponse)
     };
