74e5bdf4053dc3afb899ddbac31216952374ff16
Gabe Alena <slabsk@mail.ru>
2022-07-11 17:36:54 +0500

    try to set jwt 34

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,29 @@
-const { NODE_ENV, JWT_SECRET } = process.env;
-require('dotenv').config();
+/* const { NODE_ENV, JWT_SECRET } = process.env; */
+/* require('dotenv').config(); */
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
 
 module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new Unauthorized('Необходима авторизация!');
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    throw new Unauthorized('Необходима авторизация');
+  }
+
+  req.user = payload;
+  return next();
+};
+
+/* module.exports = (req, res, next) => {
   const authorization = req.cookies.jwt;
 
   if (!authorization) {
@@ -20,4 +40,4 @@ module.exports = (req, res, next) => {
 
   req.user = payload;
   return next();
-};
+}; */
