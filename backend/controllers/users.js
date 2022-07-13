0880207c7562278c37ae787f8628217ce5c2db46
Gabe Alena <slabsk@mail.ru>
2022-07-13 12:24:39 +0500

    cards problem 24

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -62,8 +62,8 @@ module.exports.login = (req, res, next) => {
       return res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: true,
-        secure: true,
+        /* sameSite: true, */
+        /* secure: true, */
       }).send({
         token,
         name: user.name,
