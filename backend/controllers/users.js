22847db708a25c802d3dd86247c520388fca3dfc
Gabe Alena <slabsk@mail.ru>
2022-07-15 09:53:19 +0500

    cards problem 69

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-/* const { NODE_ENV, JWT_SECRET } = process.env; */
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const User = require('../models/user');
 const ValidationError = require('../errors/ValidationError');
@@ -55,7 +55,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        'some-secret-key',
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
 
