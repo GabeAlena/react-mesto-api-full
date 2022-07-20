fd2b74ddf44a0f9f2f3d9f985392d829c678bb06
Gabe Alena <slabsk@mail.ru>
2022-07-20 15:32:52 +0500

    update site 23

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -99,7 +99,7 @@ function App() {
         Promise.all([api.getUserInfo(), api.getInitialCards()])
           .then(([userInfo, cards]) => {
             setCurrentUser(userInfo);
-            setCards(cards);
+            setCards(cards.data);
           })
           .catch((err) => {
               console.log(err);
