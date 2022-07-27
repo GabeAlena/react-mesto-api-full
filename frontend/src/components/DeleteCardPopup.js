408f69a891ee12db95f221a8f5e218748727a564
Gabe Alena <slabsk@mail.ru>
2022-07-27 12:54:11 +0500

    update site 53

---
+++ frontend/src/components/DeleteCardPopup.js
@@ -0,0 +1,20 @@
+import PopupWithForm from "./PopupWithForm";
+
+function DeleteCardPopup(props) {
+    const handleSubmit = () => {
+        props.onSubmit();
+    };
+
+    return (
+      <PopupWithForm
+        title="Вы уверены?"
+        name="card-delete"
+        textButton="Да"
+        isOpen={props.isOpen} 
+        onClose={props.onClose}
+        onSubmit={handleSubmit}
+      />  
+    );
+}
+
+export default DeleteCardPopup;
