58f037a50ad1abcbcdb4b3310ec6d7f83dba291a
Gabe Alena <slabsk@mail.ru>
2022-07-29 11:03:19 +0500

    update site 102

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -56,7 +56,7 @@ function App() {
       auth.authorization(email, password)
           .then((res) => {
             if (res.token) {
-              console.log(res.token);
+              console.log(res);
               localStorage.setItem('jwt', res.token);
               setIsLoggedIn(true);
               setUserEmail(res.email);
