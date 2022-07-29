f482a7b23a80c6b5247e8146cfc95f242aafb69d
Gabe Alena <slabsk@mail.ru>
2022-07-29 12:47:36 +0500

    update site 120

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -35,7 +35,7 @@ function App() {
     function handleRegister({ email, password }) {
       auth.register(email, password)
           .then((res) => {
-            setUserEmail(res.email);
+            setUserEmail(res.data.email);
             console.log(email);
             setInfoTooltipImage(successImage);
             setInfoTooltipMessage("Вы успешно зарегистрировались!");
@@ -77,7 +77,7 @@ function App() {
             .then((res) => {
               if (res) {
                 setCurrentUser(res);
-                setUserEmail(res.email);
+                setUserEmail(res.data.email);
                 setIsLoggedIn(true);
                 navigate('/'); 
               }
