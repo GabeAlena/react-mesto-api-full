7f592fc1fa800dc10cd905aafcfc1303050bfafd
Gabe Alena <slabsk@mail.ru>
2022-07-29 11:05:18 +0500

    update site 103

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -71,9 +71,9 @@ function App() {
     };
 
     const checkToken = () => {
-      const jwt = localStorage.getItem('jwt');
-      if (jwt) {
-        auth.checkToken(jwt)
+      const token = localStorage.getItem('jwt');
+      if (token) {
+        auth.checkToken(token)
             .then((res) => {
               if (res) {
                 setCurrentUser(res);
