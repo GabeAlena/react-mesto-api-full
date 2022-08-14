2b22239bfb98e0a2df007aa8211a0f2091713d61
Gabe Alena <slabsk@mail.ru>
2022-08-14 12:49:40 +0500

    jwt developer and production 59

--- backend/test-token.js
+++ backend/test-token.js
@@ -1,4 +1,3 @@
 const jwt = require('jsonwebtoken');
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmNiZjAwMzA0ZGU1NDRiYjlhMDg2MjAiLCJpYXQiOjE2NjA0NTg2OTUsImV4cCI6MTY2MTA2MzQ5NX0.3F28BnMpqWc-GbSHywAJIiF474eTk60cMid3qT5GG9k';
 const payload = jwt.verify(token, 'SECRET_KEY');
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,2 +1,1 @@
 export const BASE_URL = 'https://api.gabealena.students.nomoredomains.xyz';
-/* export const BASE_URL = 'http://api.gabealena.students.nomoredomains.xyz'; */
